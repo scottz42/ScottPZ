11dfd21c534bf0c5a477ed6a1519299d7cc8e52f
scottz42 <861436479@qq.com>
2017-05-06 12:27:36 +0800

    added new tasks

--- app/src/main/java/xyz/scottz/scottpz/MainActivity.java
+++ app/src/main/java/xyz/scottz/scottpz/MainActivity.java
@@ -3,6 +3,18 @@ package xyz.scottz.scottpz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+// TODO: custom view
+// TODO: draw an external image file
+// TODO: timer
+// TODO: object storage
+// TODO: new plant placement
+// TODO: object movement
+// TODO: object interaction
+// TODO: temporary object generation eg. flying beans
+//
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
