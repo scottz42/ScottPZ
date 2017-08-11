330379dfd65bb78955b529244bde24fd9d0d1c46
scottz42 <861436479@qq.com>
2017-08-12 04:58:27 +0800

    removed zombie reappearing code

--- app/src/main/java/xyz/scottz/scottpz/Zombie.java
+++ app/src/main/java/xyz/scottz/scottpz/Zombie.java
@@ -109,10 +109,7 @@ public class Zombie extends MajorObject {
         if ((System.currentTimeMillis()-LastMoveTime)>TimePerStep) {
             x -= DistancePerStep ;
             // TODO: GridLogic
-            if (x<0) {
-                x = 1000 ;
-                y = 100*((int)(Math.random()*4)+1);
-            }
+
             if (x>1200) {
                 x = 0;
             }
